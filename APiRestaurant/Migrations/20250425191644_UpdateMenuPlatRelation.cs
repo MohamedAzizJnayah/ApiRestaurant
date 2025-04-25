749abaf38504d98be1bb4cc46e5db400a8cf1404
mohamedazizjnayah <mohamedazizjnayah@gmail.com>
2025-04-25 21:02:17 +0100

    Version Stable

---
+++ APiRestaurant/Migrations/20250425191644_UpdateMenuPlatRelation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiRestaurant.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMenuPlatRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
